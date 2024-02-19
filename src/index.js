3e6de49eae7f53d262561820deca6b31af4c7b60
Alan <alanstvn420@gmail.com>
2024-02-19 15:55:02 -0500

    Backend

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+import app from './server.js'
+
+
+// Importar la función connection()
+import connection from './database.js';
+
+// Haicneod uso de la función connection()
+connection()
+
+
+app.listen(app.get('port'),()=>{
+    // console.log(`Server ok on http://localhost:${app.get('port')}`);
+    console.log(`Server ok on http://localhost:${app.get('port')}`);
+})
+
