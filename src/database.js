95ab5debf0ea154c5efd2286cd416dd15fd878a2
Alan Pérez <117743495+4lanPz@users.noreply.github.com>
2024-03-24 13:46:48 -0500

    Comentarios

--- src/database.js
+++ src/database.js
@@ -1,28 +1,23 @@
+// Importar mongoose
+import mongoose from 'mongoose'; // Importa el módulo mongoose para interactuar con la base de datos MongoDB
 
-//Importar mongoose
-import mongoose from 'mongoose'
-
-
-// Pertimitir que solo los campos definidos en el Schema sean almacenados
-// enn la BDD
-mongoose.set('strictQuery', true)
-
+// Permitir que solo los campos definidos en el Schema sean almacenados en la base de datos
+mongoose.set('strictQuery', true);
 
 // Crear una función llamada connection()
-const connection = async()=>{
+const connection = async () => {
     try {
-        // Establecer al conexión con la BDD
-        const {connection} = await mongoose.connect(process.env.MONGODB_URI)
+        // Establecer la conexión con la base de datos utilizando la URL proporcionada en la variable de entorno MONGODB_URI
+        const { connection } = await mongoose.connect(process.env.MONGODB_URI);
         
         // Presentar la conexión en consola 
-        console.log(`Database is connected on ${connection.host} - ${connection.port}`)
+        console.log(`Database is connected on ${connection.host} - ${connection.port}`);
     
     } catch (error) {
-        // Capturar Error en la conexión
+        // Capturar error en la conexión y presentarlo en consola
         console.log(error);
     }
 }
 
-
-//Exportar la función
-export default  connection
+// Exportar la función para que pueda ser utilizada en otros archivos
+export default connection;
