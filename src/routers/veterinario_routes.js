093d00087b97df55279e08781ee4591ad3dff296
Alan Pérez <117743495+4lanPz@users.noreply.github.com>
2024-03-24 14:57:09 -0500

    Comentarios

--- src/routers/veterinario_routes.js
+++ src/routers/veterinario_routes.js
@@ -1,10 +1,10 @@
 // Importar Router de Express
-import {Router} from 'express'
+import { Router } from 'express';
 
-// Crear una instancia de Router() 
-const router = Router()
+// Crear una instancia de Router()
+const router = Router();
 
-// Importar los métodos del controlador 
+// Importar métodos del controlador
 import {
     login,
     perfil,
@@ -18,55 +18,36 @@ import {
     comprobarTokenPasword,
     nuevoPassword,
 } from "../controllers/veterinario_controller.js";
+
+// Importar middleware de autenticación
 import verificarAutenticacion from '../middlewares/autenticacion.js';
 
-
+// Importar middleware de validación
 import { validacionVeterinario } from '../middlewares/validacionVeterinario.js';
 
-
-// Rutas publicas
+// Rutas públicas
 router.post("/login", login);
-
-
-
-router.post("/registro", validacionVeterinario , registro);
-
-
+router.post("/registro", validacionVeterinario, registro);
 router.get("/confirmar/:token", confirmEmail);
 router.get("/veterinarios", listarVeterinarios);
 router.post("/recuperar-password", recuperarPassword);
 router.get("/recuperar-password/:token", comprobarTokenPasword);
 router.post("/nuevo-password/:token", nuevoPassword);
 
-
-
 // Rutas privadas
-router.get("/perfil",verificarAutenticacion , perfil,);
-
-
-
-
-router.put('/veterinario/actualizarpassword',verificarAutenticacion, actualizarPassword)
-
-
-
+router.get("/perfil", verificarAutenticacion, perfil);
+router.put('/veterinario/actualizarpassword', verificarAutenticacion, actualizarPassword);
 router.get("/veterinario/:id", verificarAutenticacion, detalleVeterinario);
-
-
-
 router.put("/veterinario/:id", verificarAutenticacion, actualizarPerfil);
 
-
-
-
-
-
-
 // Exportar la variable router
-export default router
+export default router;
 
 
-
-
-
-
+/*
+Este código define las rutas para manejar las solicitudes relacionadas con los veterinarios. 
+Las rutas públicas no requieren autenticación y permiten al usuario realizar acciones como iniciar sesión, 
+registrarse, confirmar correo electrónico, listar veterinarios, recuperar contraseña y cambiar contraseña. 
+Las rutas privadas requieren autenticación y permiten al usuario acceder a su perfil, actualizar su contraseña y actualizar su perfil.
+Cada ruta está asociada a un controlador específico que maneja la lógica de negocio correspondiente. Las solicitudes HTTP POST, GET y PUT se utilizan para realizar estas operaciones.
+*/
