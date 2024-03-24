7adb63821a828ee0ddaa29f8e54a121038c5ac47
Alan Pérez <117743495+4lanPz@users.noreply.github.com>
2024-03-24 14:38:40 -0500

    Comentarios

--- src/models/Veterinario.js
+++ src/models/Veterinario.js
@@ -1,77 +1,84 @@
 // Importar el Schema y el modelo de mongoose
-import {Schema, model} from 'mongoose'
+import { Schema, model } from 'mongoose';
 // Importar bcrypt para cifrar las contraseñas
-import bcrypt from "bcryptjs"
-
+import bcrypt from "bcryptjs";
 
 // Crear el Schema "atributos de la tabla de la BDD"
 const veterinarioSchema = new Schema({
-    nombre:{
-        type:String,
-        require:true,
-        trim:true
+    // Campo para el nombre del veterinario
+    nombre: {
+        type: String,
+        required: true,
+        trim: true
     },
-    apellido:{
-        type:String,
-        require:true,
-        trim:true
+    // Campo para el apellido del veterinario
+    apellido: {
+        type: String,
+        required: true,
+        trim: true
     },
-    direccion:{
-        type:String,
-        trim:true,
-        default:null
+    // Campo para la dirección del veterinario
+    direccion: {
+        type: String,
+        trim: true,
+        default: null
     },
-    telefono:{
-        type:Number,
-        trim:true,
-        default:null
+    // Campo para el número de teléfono del veterinario
+    telefono: {
+        type: Number,
+        trim: true,
+        default: null
     },
-    email:{
-        type:String,
-        require:true,
-        trim:true,
-		unique:true
+    // Campo para el correo electrónico del veterinario
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true // El correo electrónico debe ser único
     },
-    password:{
-        type:String,
-        require:true
+    // Campo para la contraseña cifrada del veterinario
+    password: {
+        type: String,
+        required: true
     },
-    status:{
-        type:Boolean,
-        default:true
+    // Campo para el estado del veterinario (activo o inactivo)
+    status: {
+        type: Boolean,
+        default: true
     },
-    token:{
-        type:String,
-        default:null
+    // Campo para el token de confirmación del correo electrónico
+    token: {
+        type: String,
+        default: null
     },
-    confirmEmail:{
-        type:Boolean,
-        default:false
+    // Campo para indicar si el correo electrónico del veterinario ha sido confirmado
+    confirmEmail: {
+        type: Boolean,
+        default: false
     }
-
-},{
-    timestamps:true
-})
+}, {
+    timestamps: true // Agregar timestamps de creación y modificación automáticamente
+});
 
 // Método para cifrar el password del veterinario
-veterinarioSchema.methods.encrypPassword = async function(password){
-    const salt = await bcrypt.genSalt(10)
-    const passwordEncryp = await bcrypt.hash(password,salt)
-    return passwordEncryp
-}
+veterinarioSchema.methods.encrypPassword = async function(password) {
+    const salt = await bcrypt.genSalt(10);
+    const passwordEncryp = await bcrypt.hash(password, salt);
+    return passwordEncryp;
+};
 
 // Método para verificar si el password ingresado es el mismo de la BDD
-veterinarioSchema.methods.matchPassword = async function(password){
-    const response = await bcrypt.compare(password,this.password)
-    return response
-}
+veterinarioSchema.methods.matchPassword = async function(password) {
+    const response = await bcrypt.compare(password, this.password);
+    return response;
+};
 
 // Método para crear un token 
-veterinarioSchema.methods.crearToken = function(){
-    const tokenGenerado = this.token = Math.random().toString(36).slice(2)
-    return tokenGenerado
-}
+veterinarioSchema.methods.crearToken = function() {
+    const tokenGenerado = this.token = Math.random().toString(36).slice(2);
+    return tokenGenerado;
+};
 
 // Crear el Modelo Veterinario "Tabla BDD" en base al esquema llamado veterinarioSchema
 // Luego exportar el modelo
-export default model('Veterinario',veterinarioSchema)
+export default model('Veterinario', veterinarioSchema);
