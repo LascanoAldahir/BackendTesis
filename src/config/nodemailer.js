f9b9d99980e0cb45aebfa17e94a6f5da921bcf6d
Alan Pérez <117743495+4lanPz@users.noreply.github.com>
2024-03-24 13:51:41 -0500

    Comentarios

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,80 +1,74 @@
-import nodemailer from "nodemailer"
-import dotenv from 'dotenv'
-dotenv.config()
+import nodemailer from "nodemailer"; // Importa el módulo nodemailer para enviar correos electrónicos
+import dotenv from 'dotenv'; // Importa el módulo dotenv para cargar variables de entorno desde un archivo '.env'
+dotenv.config(); // Carga las variables de entorno desde el archivo .env
 
-
+// Configura el transporte de correo con los datos proporcionados en las variables de entorno
 let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: process.env.HOST_MAILTRAP,
-    port: process.env.PORT_MAILTRAP,
+    service: 'gmail', // Servicio de correo electrónico a utilizar (en este caso, Gmail)
+    host: process.env.HOST_MAILTRAP, // Host del servicio de correo electrónico (obtenido de las variables de entorno)
+    port: process.env.PORT_MAILTRAP, // Puerto del servicio de correo electrónico (obtenido de las variables de entorno)
     auth: {
-        user: process.env.USER_MAILTRAP,
-        pass: process.env.PASS_MAILTRAP,
+        user: process.env.USER_MAILTRAP, // Nombre de usuario del servicio de correo electrónico (obtenido de las variables de entorno)
+        pass: process.env.PASS_MAILTRAP, // Contraseña del servicio de correo electrónico (obtenida de las variables de entorno)
     }
 });
 
-
-const sendMailToUser = async(userMail,token)=>{
+// Función para enviar un correo electrónico de verificación al usuario
+const sendMailToUser = async (userMail, token) => {
+    // let es una variable que ouede cambiar por eso no es const
     let info = await transporter.sendMail({
-    from: 'admin@vet.com',
-    to: userMail,
-    subject: "Verifica tu cuenta de correo electrónico",
-    html: `
-    <h1>Sistema de gestión (VET-ESFOT 🐶 😺)</h1>
-    <hr>
-    <a href=${process.env.URL_FRONTEND}/confirmar/${token}>Clic para confirmar tu cuenta</a>
-    <hr>
-    <footer>Grandote te da la Bienvenida!</footer>
-    `
+        from: 'admin@vet.com', // Dirección de correo electrónico del remitente
+        to: userMail, // Dirección de correo electrónico del destinatario
+        subject: "Verifica tu cuenta de correo electrónico", // Asunto del correo electrónico
+        html: `
+        <h1>Sistema de gestión (VET-ESFOT 🐶 😺)</h1>
+        <hr>
+        <a href=${process.env.URL_FRONTEND}/confirmar/${token}>Clic para confirmar tu cuenta</a>
+        <hr>
+        <footer>Grandote te da la Bienvenida!</footer>
+        `
+    });
+    console.log("Mensaje enviado satisfactoriamente: ", info.messageId); // Imprime el ID del mensaje enviado satisfactoriamente en consola
+}
+
+// Función para enviar un correo electrónico de recuperación de contraseña al usuario
+const sendMailToRecoveryPassword = async (userMail, token) => {
+    let info = await transporter.sendMail({
+        from: 'admin@vet.com',
+        to: userMail,
+        subject: "Correo para reestablecer tu contraseña",
+        html: `
+        <h1>Sistema de gestión (VET-ESFOT 🐶 😺)</h1>
+        <hr>
+        <a href=${process.env.URL_FRONTEND}/recuperar-password/${token}>Clic para reestablecer tu contraseña</a>
+        <hr>
+        <footer>Grandote te da la Bienvenida!</footer>
+        `
     });
     console.log("Mensaje enviado satisfactoriamente: ", info.messageId);
 }
 
-
-// send mail with defined transport object
-const sendMailToRecoveryPassword = async(userMail,token)=>{
+// Función para enviar un correo electrónico de bienvenida al paciente
+const sendMailToPaciente = async (userMail, password) => {
     let info = await transporter.sendMail({
-    from: 'admin@vet.com',
-    to: userMail,
-    subject: "Correo para reestablecer tu contraseña",
-    html: `
-    <h1>Sistema de gestión (VET-ESFOT 🐶 😺)</h1>
-    <hr>
-    <a href=${process.env.URL_FRONTEND}/recuperar-password/${token}>Clic para reestablecer tu contraseña</a>
-    <hr>
-    <footer>Grandote te da la Bienvenida!</footer>
-    `
+        from: 'admin@vet.com',
+        to: userMail,
+        subject: "Correo de bienvenida",
+        html: `
+        <h1>Sistema de gestión (VET-ESFOT 🐶 😺)</h1>
+        <hr>
+        <p>Contraseña de acceso: ${password}</p>
+        <a href=${process.env.URL_BACKEND}/paciente/login>Clic para iniciar sesión</a>
+        <hr>
+        <footer>Grandote te da la Bienvenida!</footer>
+        `
     });
     console.log("Mensaje enviado satisfactoriamente: ", info.messageId);
 }
 
-
-
-
-// send mail to patient
-const sendMailToPaciente = async(userMail,password)=>{
-    let info = await transporter.sendMail({
-    from: 'admin@vet.com',
-    to: userMail,
-    subject: "Correo de bienvenida",
-    html: `
-    <h1>Sistema de gestión (VET-ESFOT 🐶 😺)</h1>
-    <hr>
-    <p>Contraseña de acceso: ${password}</p>
-    <a href=${process.env.URL_BACKEND}/paciente/login>Clic para iniciar sesión</a>
-    <hr>
-    <footer>Grandote te da la Bienvenida!</footer>
-    `
-    });
-    console.log("Mensaje enviado satisfactoriamente: ", info.messageId);
-}
-
-
-
+// Exporta las funciones para que puedan ser utilizadas en otros archivos
 export {
     sendMailToUser,
     sendMailToRecoveryPassword,
     sendMailToPaciente
 }
-
-
