ffca475ef5bec364bf4590db45719d2c3313941a
Alan Pérez <117743495+4lanPz@users.noreply.github.com>
2024-03-24 14:12:34 -0500

    Comentarios

--- src/helpers/crearJWT.js
+++ src/helpers/crearJWT.js
@@ -1,12 +1,9 @@
 import jwt from "jsonwebtoken";
 
-
-
-const generarJWT = (id,rol)=>{
-
-    return jwt.sign({id,rol},process.env.JWT_SECRET,{expiresIn:"1d"})
+// Función para generar un JSON Web Token (JWT) con el ID y el rol proporcionados
+const generarJWT = (id, rol) => {
+    // Genera un JWT con el ID y el rol, utilizando el secreto JWT definido en las variables de entorno y configurando su expiración en 1 día
+    return jwt.sign({ id, rol }, process.env.JWT_SECRET, { expiresIn: "1d" });
 }
 
-
-export default  generarJWT
-
+export default generarJWT; // Exporta la función de generación de JWT
