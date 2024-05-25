f4e71df634410ef3141f3f204327d43511885f6f
LascanoAldahir <aldahirlascano@gmail.com>
2024-05-24 20:55:33 -0500

    ordentrabajo

--- src/controllers/ordentrabajo_controller.js
+++ src/controllers/ordentrabajo_controller.js
@@ -15,93 +15,6 @@ const buscarClientePorCedula = async (req, res) => {
     }
   };
 
-  const verificarEstadoYMostrarModal = async (req, res) => {
-    try {
-        const { id } = req.params;
-
-        // Buscar el equipo por su ID
-        const equipo = await Equipo.findById(id);
-
-        if (!equipo) {
-            return res.status(404).json({ message: 'Equipo no encontrado' });
-        }
-
-        // Verificar el estado del servicio
-        if (equipo.servicio === 'reparación') {
-            // Aquí puedes implementar la lógica para mostrar el modal de proforma
-            // En un entorno de backend puro, esto podría implicar devolver una respuesta
-            // que indique al frontend que debe mostrar el modal
-
-            // Ejemplo de respuesta JSON que indica que se debe mostrar el modal
-            return res.status(200).json({ mostrarModal: true, message: 'Mostrar modal de proforma' });
-        } else {
-            return res.status(200).json({ mostrarModal: false, message: 'No es necesario mostrar el modal de proforma' });
-        }
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Error del servidor' });
-    }
-};
-
-// Método para ver el detalle del tratamiento
-const detalleEquipo = async(req,res)=>{
-    const {id} = req.params // Extrae el ID del tratamiento de los parámetros de la solicitud
-    // Verifica si el ID del tratamiento es válido
-    if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe ese equipo`});
-    // Busca el equipo por su ID y lo popula con la información del paciente asociado
-    const equipo = await Equipo.findById(id).populate('cliente','_id nombre')
-    // Responde con el detalle del equipo
-    res.status(200).json(equipo)
-}
-
-// Método para crear el equipo
-const registrarEquipo = async (req,res)=>{
-    const {paciente} = req.body // Extrae el ID del paciente del cuerpo de la solicitud
-    // Verifica si el ID del paciente es válido
-    if( !mongoose.Types.ObjectId.isValid(paciente) ) return res.status(404).json({msg:`Lo sentimos, debe ser un id válido`});
-    // Crea un nuevo tratamiento con los datos proporcionados en el cuerpo de la solicitud
-    const tratamiento = await Tratamiento.create(req.body)
-    // Responde con un mensaje de éxito y el tratamiento creado
-    res.status(200).json({msg:`Registro exitoso del tratamiento ${tratamiento._id}`,tratamiento})
-}
-
-//metodo para elegir tipo de trabajo a realizar.
-
-
-// Método para actualizar el tratamiento
-const actualizarEquipo = async(req,res)=>{
-    const {id} = req.params // Extrae el ID del tratamiento de los parámetros de la solicitud
-    // Verifica si algún campo del cuerpo de la solicitud está vacío
-    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
-    // Verifica si el ID del tratamiento es válido
-    if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe el tratamiento ${id}`})
-    // Busca y actualiza el tratamiento en la base de datos utilizando el ID proporcionado
-    await Tratamiento.findByIdAndUpdate(req.params.id,req.body)
-    // Responde con un mensaje de éxito
-    res.status(200).json({msg:"Actualización exitosa del tratamiento"})
-}
-
-// Método para eliminar el tratamiento
-const eliminarEquipo = async(req,res)=>{
-    const {id} = req.params // Extrae el ID del tratamiento de los parámetros de la solicitud
-    // Verifica si algún campo del cuerpo de la solicitud está vacío
-    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
-    // Verifica si el ID del tratamiento es válido
-    if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe ese tratamiento`})
-    // Busca y elimina el tratamiento de la base de datos utilizando el ID proporcionado
-    await Tratamiento.findByIdAndDelete(req.params.id)
-    // Responde con un mensaje de éxito
-    res.status(200).json({msg:"Tratamiento eliminado exitosamente"})
-}
-
-// Método para cambiar el estado del tratamiento
-const cambiarEstado = async(req,res)=>{
-    // Actualiza el estado del tratamiento en la base de datos estableciendo el estado en false
-    await Tratamiento.findByIdAndUpdate(req.params.id,{estado:false})
-    // Responde con un mensaje de éxito
-    res.status(200).json({msg:"Estado del Tratamiento modificado exitosamente"})
-}
-
 // Método para agregar un tipo de servicio a un equipo
 const tipoServicio = async (req, res) => {
     try {
@@ -133,11 +46,6 @@ const tipoServicio = async (req, res) => {
 
 // Exporta los métodos de la API relacionados con la gestión de tratamientos
 export {
-    detalleEquipo,
-    registrarEquipo,
-    actualizarEquipo,
-    eliminarEquipo,
     buscarClientePorCedula,
-    cambiarEstado,
     tipoServicio
 }
--- src/routers/ordentrabajo_routes.js
+++ src/routers/ordentrabajo_routes.js
@@ -1,65 +1,17 @@
 import { Router } from 'express';
 // Importar el modelo de equipo
-import Equipo from '../models/Equipo.js';
+import ordentrabajo from '../models/ordentrabajo.js';
 
 const router = Router();
 
 // Importar controladores de tratamientos
 import {
-    detalleEquipo,
-    registrarEquipo,
-    actualizarEquipo,
-    buscarClientePorCedula,
-    eliminarEquipo,
-    cambiarEstado,
-    tipoServicio 
-} from "../controllers/equipo_controller.js";
+    buscarClientePorCedula, 
+} from "../controllers/ordentrabajo_controller.js";
 
 // Importar middleware de autenticación
 import verificarAutenticacion from "../middlewares/autenticacion.js";
 
-// Rutas para el manejo de Equipos
-// Ruta para registrar un nuevo Equipos
-router.post('/equipo/registro', verificarAutenticacion, registrarEquipo);
-
-// Ruta para obtener el detalle de un tratamiento específico
-router.get('/equipo/:id', verificarAutenticacion, detalleEquipo);
-
 router.get('/clientes/cedula/:cedula', verificarAutenticacion,buscarClientePorCedula);
-
-// Ruta para actualizar el estado de un equipo
-// router.put('/tratamiento/:id',verificarAutenticacion,actualizarTratamiento)
-router.put('/equipo/estado/:id', verificarAutenticacion, actualizarEquipo);
-
-// Ruta para eliminar un tratamiento
-router.delete('/equipo/:id', verificarAutenticacion, eliminarEquipo);
-
-// Ruta para cambiar el estado de un tratamiento
-router.post('/equipo/estado/:id', verificarAutenticacion, cambiarEstado);
-
-//Ruta para definir el tipo de servicio 
-router.put('/equipo/:id/tiposervicio', tipoServicio);
-
-// Ruta para mostrar el formulario de proforma para un cliente específico
-router.get('/cliente/:id/proforma', async (req, res) => {
-    try {
-        const { id } = req.params;
-        // Buscar el equipo del cliente
-        const equipo = await Equipo.findById(id);
-        if (!equipo) {
-            return res.status(404).json({ msg: 'Equipo no encontrado' });
-        }
-        // Si el tipo de servicio es reparación, mostrar el formulario de proforma
-        if (equipo.tipoServicio === 'Reparación') {
-            // Renderizar el formulario de proforma para el cliente
-            res.render('formulario_proforma_cliente', { equipo });
-        } else {
-            res.status(400).json({ msg: 'El equipo no está en reparación' });
-        }
-    } catch (error) {
-        console.error('Error al obtener el equipo:', error);
-        res.status(500).json({ msg: 'Error del servidor' });
-    }
-});
 
 export default router; // Exportar el enrutador
