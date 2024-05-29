cf389ee26dd60c4e115e3158200d01dc00d67aa9
LascanoAldahir <aldahirlascano@gmail.com>
2024-05-28 20:16:11 -0500

    index limpio

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 // Importa la instancia de la aplicación express desde el archivo "server.js"
 import app from "./server.js";
-import http from "http"; // Importa el módulo 'http' para crear el servidor HTTP
-import { Server } from "socket.io"; // Importa la clase 'Server' de socket.io para crear el servidor de WebSockets
 import ordenesRoutes from './routers/ordentrabajo_routes.js';
-
 // Importar la función connection()
 import connection from "./database.js"; // Importa la función 'connection' desde el archivo "database.js" para establecer la conexión con la base de datos
 
 // Haciendo uso de la función connection()
 connection(); // Llama a la función 'connection' para establecer la conexión con la base de datos
-
-const server = http.createServer(app); // Crea un servidor HTTP utilizando la instancia de la aplicación express
-
-const io = new Server(server, { // Crea una instancia del servidor de WebSockets y lo adjunta al servidor HTTP
-  cors: {
-    origin: "http://localhost:3000", // Configura las opciones de cors permitiendo solicitudes solo desde "http://localhost:5173"
-  },
-});
 
 // Usar las rutas de órdenes de trabajo
 app.use('/api', ordenesRoutes);
