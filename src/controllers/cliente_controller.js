22007f6e8bc4222a6b5bda5c808b5121f40c2789
LascanoAldahir <aldahirlascano@gmail.com>
2024-05-29 10:12:57 -0500

    .

--- src/controllers/cliente_controller.js
+++ src/controllers/cliente_controller.js
@@ -63,10 +63,6 @@ const loginCliente = async (req, res) => {
   }
 };
 
-export { loginCliente };
-
-
-export { loginCliente };
 
 //----------------------------------------------------------------------------------------------------
 
