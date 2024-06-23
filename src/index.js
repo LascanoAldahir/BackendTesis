e8fda1aec49e9e8308964ef804dadac148c09370
LascanoAldahir <aldahirlascano@gmail.com>
2024-06-22 21:17:08 -0500

    ELIMINACION EN INDEX DE ORDENES

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 // Importa la instancia de la aplicación express desde el archivo "server.js"
 import app from "./server.js";
-import ordenesRoutes from './routers/ordentrabajo_routes.js';
+
 // Importar la función connection()
 import connection from "./database.js"; // Importa la función 'connection' desde el archivo "database.js" para establecer la conexión con la base de datos
 
 // Haciendo uso de la función connection()
 connection(); // Llama a la función 'connection' para establecer la conexión con la base de datos
 
-// Usar las rutas de órdenes de trabajo
-app.use('/api', ordenesRoutes);
 
 // Escucha las conexiones entrantes en el puerto configurado en la aplicación express
 app.listen(app.get("port"), () => {
