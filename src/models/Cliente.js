9072f1c6cdf2bc3dd2b5758606aa4979559ebac9
LascanoAldahir <aldahirlascano@gmail.com>
2024-07-10 14:00:16 -0500

    modelo corregido por doble mencion al schema

--- src/models/Cliente.js
+++ src/models/Cliente.js
@@ -1,6 +1,8 @@
-import mongoose, { Schema, model } from 'mongoose'; // Importa mongoose para la definición del esquema y el modelo
+import mongoose from 'mongoose'; // Importa mongoose para la definición del esquema y el modelo
 // Importar bcrypt para cifrar las contraseñas
 import bcrypt from "bcryptjs";
+const { Schema, model } = mongoose;
+
 // Define el esquema del paciente
 const clienteSchema = new Schema({
     nombre: {
@@ -40,23 +42,24 @@ const clienteSchema = new Schema({
         ref: 'Tecnico' // Nombre del modelo Tecnico
     },
     token: { 
-        type: String },
-  tokenExpires: { 
+        type: String
+     },
+    tokenExpires: { 
     type: Date },
 }, 
 );
 
 // Método para cifrar el password del cliente
-ClienteSchema.methods.encryptPassword = async function (password) {
+clienteSchema.methods.encryptPassword = async function (password) {
     const salt = await bcrypt.genSalt(10);
     return await bcrypt.hash(password, salt);
-  };
+};
 
 // Método para verificar si el password ingresado es el mismo de la BDD
-ClienteSchema.methods.matchPassword = async function (password) {
+clienteSchema.methods.matchPassword = async function (password) {
     return await bcrypt.compare(password, this.password);
-  };
+};
 
-  const Cliente = mongoose.model('Cliente', ClienteSchema);
+  
 
 export default model('Cliente', clienteSchema); // Exporta el modelo de datos del paciente
